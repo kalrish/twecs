22936697d2ba5f47641283de67ef35095f9871e3
David Joaquín Shourabi Porcel <david@djsp.eu>
2022-07-19 07:10:18 +0200

    Commit files created by command `cdk init`
    version as reported by `cdk --version`: 2.32.1 (build 79cbe95)

---
+++ test/twecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Twecs from '../lib/twecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/twecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Twecs.TwecsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
