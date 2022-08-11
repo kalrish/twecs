e4fe3ff1d3c3bcbccfc956a415bc7004a48a8841
David Joaquín Shourabi Porcel <david@djsp.eu>
2022-08-11 07:01:50 +0200

    Indent Python code with tabs instead of spaces
    Yes, I know, I'm going to hell. Satan's gonna be fucked.

--- tests/unit/test_twecs_stack.py
+++ tests/unit/test_twecs_stack.py
@@ -6,12 +6,12 @@ import twecs.twecs_stack
 
 def test_sqs_queue_created(
 ):
-    app = aws_cdk.App(
-    )
-    stack = twecs.twecs_stack.TwecsStack(
-        app,
-        "twecs",
-    )
-    template = aws_cdk.assertions.Template.from_stack(
-        stack,
-    )
+	app = aws_cdk.App(
+	)
+	stack = twecs.twecs_stack.TwecsStack(
+		app,
+		"twecs",
+	)
+	template = aws_cdk.assertions.Template.from_stack(
+		stack,
+	)
--- twecs/twecs_stack.py
+++ twecs/twecs_stack.py
@@ -3,18 +3,18 @@ import constructs
 
 
 class TwecsStack(
-    aws_cdk.Stack,
+	aws_cdk.Stack,
 ):
 
-    def __init__(
-        self,
-        scope: constructs.Construct,
-        construct_id: str,
-        **kwargs,
-    ) -> None:
-        super(
-        ).__init__(
-            scope,
-            construct_id,
-            **kwargs,
-        )
+	def __init__(
+		self,
+		scope: constructs.Construct,
+		construct_id: str,
+		**kwargs,
+	) -> None:
+		super(
+		).__init__(
+			scope,
+			construct_id,
+			**kwargs,
+		)
