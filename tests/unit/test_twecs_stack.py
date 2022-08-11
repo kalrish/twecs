20b8def661f5215fbc0863aac218c5db7d45b630
David Joaquín Shourabi Porcel <david@djsp.eu>
2022-08-11 04:53:12 +0200

    Commit files created by `cdk init`
    CDK version: 2.37.1 build f15dee0

---
+++ tests/unit/test_twecs_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from twecs.twecs_stack import TwecsStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in twecs/twecs_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = TwecsStack(app, "twecs")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
