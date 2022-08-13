dc876d2691dc9a958e12ad30293db633364091d3
David Joaquín Shourabi Porcel <david@djsp.eu>
2022-08-13 16:25:08 +0200

    Deploy app onto my personal account in the region `us-east-1`

--- app.py
+++ app.py
@@ -9,5 +9,23 @@ import twecs.twecs_stack
 app = aws_cdk.App(
 )
 
+Wise_profile_IDs = {
+	twecs.twecs_stack.WiseEnvironment.LIVE: "13914146",
+	twecs.twecs_stack.WiseEnvironment.SANDBOX: "16206006",
+}
+
+Wise_environment = twecs.twecs_stack.WiseEnvironment.SANDBOX
+
+twecs.twecs_stack.CoreStack(
+	app,
+	"TwecsStack",
+	env=aws_cdk.Environment(
+		account="659664769788",
+		region="us-east-1",
+	),
+	Wise_API_host=twecs.twecs_stack.WiseEnvironment.SANDBOX,
+	Wise_profile_ID=Wise_profile_IDs[Wise_environment],
+)
+
 app.synth(
 )
